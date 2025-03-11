8c765058292ae862f513c09caae157ee46ee9172
Lasha Daushvili <ldaushvili@tbcbank.com.ge>
2025-03-11 11:51:41 +0400

    add divide

--- StrykerApp/Calculator.cs
+++ StrykerApp/Calculator.cs
@@ -22,5 +22,16 @@ namespace StrykerApp
         {
             return num1 * num2;
         }
+        public double Divide(double num1, double num2)
+        {
+            if (num2 != 0)
+            {
+                return num1 / num2;
+            }
+            else
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+        }
     }
 }
