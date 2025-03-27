8d248f7eae3269d0c55e2f0be2c1ab3ae8e80b12
Lasha Daushvili <ldaushvili@tbcbank.com.ge>
2025-03-27 16:20:44 +0400

    leave only divide

--- StrykerApp/Calculator2.cs
+++ StrykerApp/Calculator2.cs
@@ -2,21 +2,6 @@
 
 public class Calculator2
 {
-    public double Add(double num1, double num2)
-    {
-        return num1 + num2;
-    }
-
-    public double Subtract(double num1, double num2)
-    {
-        return num1 - num2;
-    }
-
-    public double Multiply(double num1, double num2)
-    {
-        return num1 * num2;
-    }
-
     public double Divide(double num1, double num2)
     {
         if (num2 == 0)
